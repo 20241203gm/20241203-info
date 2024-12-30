cf4339ebe74e87095e395b293ab4a45a34828e34
REDOUT-PC\user <redoutk@gmail.com>
2024-12-30 17:04:10 +0900

    Next.js 버전 다운그레이드 및 페이지 수정

--- src/app/archive/page.tsx
+++ src/app/archive/page.tsx
@@ -1,7 +1,13 @@
-export default function Page() {
+import { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Archive',
+}
+
+export default async function Page() {
   return (
     <div>
       <h1>Archive Page</h1>
     </div>
-  );
+  )
 }
